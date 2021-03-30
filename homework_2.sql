86185e01cfaa5713d6747cd0dd8748610a5c63d0
LarkovAA <76556307+LarkovAA@users.noreply.github.com>
2021-03-31 00:07:26 +0400

    Сделал 3 задания

---
+++ homework_2.sql
@@ -0,0 +1,24 @@
+/* Задание 2 Создайте базу данных example, разместите в ней таблицу users, 
+состоящую из двух столбцов, числового id и строкового name*/
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(255)
+) ;
+
+SELECT * FROM users;
+
+/* Задание 3 Создайте дамп базы данных example из предыдущего задания, 
+ * разверните содержимое дампа в новую базу данных sample 
+
+mysqldump (example) > dump_in_sample;
+mysql dump_in_sample < sample; 
+
+ * */
+
+
+
+
+
+
