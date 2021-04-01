061cd29b991e209a9cb7516f90f47adcb29f4035
LarkovAA <76556307+LarkovAA@users.noreply.github.com>
2021-04-01 20:02:54 +0400

    Переделал ДЗ по 2 уроку.
    Переделал ДЗ по 2 уроку.

--- homework_2.sql
+++ homework_2.sql
@@ -1,6 +1,8 @@
 /* Задание 2 Создайте базу данных example, разместите в ней таблицу users, 
 состоящую из двух столбцов, числового id и строкового name*/
 
+CREATE DATABASE IF NOT EXISTS example;
+
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
 id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -12,7 +14,9 @@ SELECT * FROM users;
 /* Задание 3 Создайте дамп базы данных example из предыдущего задания, 
  * разверните содержимое дампа в новую базу данных sample 
 
-mysqldump (example) > dump_in_sample;
+CREATE DATABASE IF NOT EXISTS samplee;
+
+mysqldump example > dump_in_sample;
 mysql dump_in_sample < sample; 
 
  * */
